d4204ccd90f7ba95b7aa2968467ca8e98f4c8d19
Stuart Mitchell <hg@stuartmitchell.com>
2011-10-16 20:18:18 +1300

    added support for CPLEX_PY solver that uses the python binding for cplex

--- src/pulp/tests.py
+++ src/pulp/tests.py
@@ -106,7 +106,7 @@ def pulpTest012(solver):
     prob += -y+z == 7, "c3"
     prob += w >= 0, "c4"
     print "\t Testing unbounded continuous LP solution"
-    if solver.__class__ in [GUROBI, CPLEX_CMD, YAPOSIB]:
+    if solver.__class__ in [GUROBI, CPLEX_CMD, YAPOSIB, CPLEX_PY]:
         # These solvers report infeasible or unbounded
         pulpTestCheck(prob, solver, [LpStatusInfeasible])
     elif solver.__class__ in [COINMP_DLL,]:
@@ -157,7 +157,8 @@ def pulpTest014(solver):
     prob += -y+z == 7, "c3"
     prob += w >= 0, "c4"
     print "\t Testing repeated Names"
-    if solver.__class__ in [COIN_CMD, PULP_CBC_CMD, CPLEX_CMD, GLPK_CMD]:
+    if solver.__class__ in [COIN_CMD, PULP_CBC_CMD, CPLEX_CMD, CPLEX_PY,
+            GLPK_CMD]:
         try:
             pulpTestCheck(prob, solver, [LpStatusOptimal], {x:4, y:-1, z:6, w:0})
         except PulpError:
@@ -450,7 +451,7 @@ def pulpTest123(solver):
     prob.extend((w >= -1).makeElasticSubProblem(penalty = 0.9))
     print "\t Testing elastic constraints (penalty unbounded)"
     prob.writeLP('debug.lp')
-    if solver.__class__ in [COINMP_DLL, GUROBI, CPLEX_CMD, YAPOSIB]:
+    if solver.__class__ in [COINMP_DLL, GUROBI, CPLEX_CMD, CPLEX_PY, YAPOSIB]:
         # COINMP_DLL Does not report unbounded problems, correctly
          pulpTestCheck(prob, solver, [LpStatusInfeasible])
     elif solver.__class__ is GLPK_CMD:
@@ -480,4 +481,3 @@ def pulpTestSolver(solver, msg = 0):
             ]
     for t in tests:
         t(solver(msg=msg))
-
