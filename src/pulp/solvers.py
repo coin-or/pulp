2bbb382f8d7a1029aac86b7154c32eb64fe6f9ec
stu <stu@stuartmitchell.com>
2014-07-02 11:37:17 +1200

    fix for gurobi cmd and extra options http://code.google.com/p/pulp-or/issues/detail?id=64

--- src/pulp/solvers.py
+++ src/pulp/solvers.py
@@ -1888,7 +1888,7 @@ class GUROBI_CMD(LpSolver_CMD):
         except: pass
         cmd = self.path
         cmd += ' ' + ' '.join(['%s=%s' % (key, value)
-                    for key, value in self.options])
+                    for key, value in self.options.items()])
         cmd += ' ResultFile=%s' % tmpSol
         if lp.isMIP():
             if not self.mip:
