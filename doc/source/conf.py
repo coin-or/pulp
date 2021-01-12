995d86493cde7e4ff1929dd6b39b3c072639e061
Riley Clement <rclement@hvccc.com.au>
2021-01-13 09:14:30 +1100

    Replaced sphinx_glpi_theme with sphinx_rtd_theme in docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,9 +22,18 @@ sys.path.insert(0,os.path.abspath('../src/pulp/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 
-'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 
-'sphinx.ext.ifconfig', 'sphinx.ext.autosummary', 'sphinx.ext.mathjax']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx_rtd_theme",
+]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -103,13 +112,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-# html_theme = 'default'
-# html_theme = 'sphinx-glpi-theme'
-import sphinx_glpi_theme
+import sphinx_rtd_theme
 
-html_theme = "glpi"
+html_theme = "sphinx_rtd_theme"
 
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
