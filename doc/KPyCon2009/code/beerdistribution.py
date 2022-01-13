5465714ab472d3e2c75f5f44a3e63974a4f6d5c3
Sam <sam.cfd.iitm@gmail.com>
2022-01-13 19:08:32 +0530

    Black applied and a few code corrections!

--- doc/KPyCon2009/code/beerdistribution.py
+++ doc/KPyCon2009/code/beerdistribution.py
@@ -11,54 +11,56 @@ import pulp
 warehouses = ["A", "B"]
 
 # Creates a dictionary for the number of units of supply for each supply node
-supply = {"A": 1000,
-          "B": 4000}
+supply = {"A": 1000, "B": 4000}
 
 # Creates a list of all demand nodes
 bars = ["1", "2", "3", "4", "5"]
 
 # Creates a dictionary for the number of units of demand for each demand node
-demand = {"1":500,
-          "2":900,
-          "3":1800,
-          "4":200,
-          "5":700,}
+demand = {
+    "1": 500,
+    "2": 900,
+    "3": 1800,
+    "4": 200,
+    "5": 700,
+}
 
 # Creates a list of costs of each transportation path
-costs = [   #Bars
-         #1 2 3 4 5
-         [2,4,5,2,1],#A   Warehouses
-         [3,1,3,2,3] #B
-         ]
+costs = [  # Bars
+    # 1 2 3 4 5
+    [2, 4, 5, 2, 1],  # A   Warehouses
+    [3, 1, 3, 2, 3],  # B
+]
 
 # The cost data is made into a dictionary
-costs = pulp.makeDict([warehouses, bars], costs,0)
+costs = pulp.makeDict([warehouses, bars], costs, 0)
 
 # Creates the 'prob' variable to contain the problem data
 prob = pulp.LpProblem("Beer Distribution Problem", pulp.LpMinimize)
 
 # Creates a list of tuples containing all the possible routes for transport
-routes = [(w,b) for w in warehouses for b in bars]
+routes = [(w, b) for w in warehouses for b in bars]
 
 # A dictionary called x is created to contain quantity shipped on the routes
-x = pulp.LpVariable.dicts("route", (warehouses, bars), 
-                        lowBound = 0 
-                        cat = pulp.LpInteger)
+x = pulp.LpVariable.dicts("route", (warehouses, bars), lowBound=0, cat=pulp.LpInteger)
 
 # The objective function is added to 'prob' first
-prob += sum([x[w][b]*costs[w][b] for (w,b) in routes]), \
-            "Sum_of_Transporting_Costs"
+prob += sum([x[w][b] * costs[w][b] for (w, b) in routes]), "Sum_of_Transporting_Costs"
 
 # Supply maximum constraints are added to prob for each supply node (warehouse)
 for w in warehouses:
-    prob += sum([x[w][b] for b in bars]) <= supply[w], \
-            "Sum_of_Products_out_of_Warehouse_%s"%w
+    prob += (
+        sum([x[w][b] for b in bars]) <= supply[w],
+        "Sum_of_Products_out_of_Warehouse_%s" % w,
+    )
 
 # Demand minimum constraints are added to prob for each demand node (bar)
 for b in bars:
-    prob += sum([x[w][b] for w in warehouses]) >= demand[b], \
-    "Sum_of_Products_into_Bar%s"%b
-                   
+    prob += (
+        sum([x[w][b] for w in warehouses]) >= demand[b],
+        "Sum_of_Products_into_Bar%s" % b,
+    )
+
 # The problem data is written to an .lp file
 prob.writeLP("BeerDistributionProblem.lp")
 
@@ -66,11 +68,11 @@ prob.writeLP("BeerDistributionProblem.lp")
 prob.solve()
 
 # The status of the solution is printed to the screen
-print "Status:", pulp.LpStatus[prob.status]
+print("Status:", pulp.LpStatus[prob.status])
 
 # Each of the variables is printed with it's resolved optimum value
 for v in prob.variables():
-    print v.name, "=", v.varValue
+    print(v.name, "=", v.varValue)
 
-# The optimised objective function value is printed to the screen    
-print "Total Cost of Transportation = ", prob.objective.value()
+# The optimised objective function value is printed to the screen
+print("Total Cost of Transportation = ", prob.objective.value())
