4f86580b23e5e892a7461b95baabfe133dacb3cf
Franco Peschiera <pchtsp@gmail.com>
2022-04-28 20:06:36 +0200

    mps support for empty name (#542)
    fixes #539

--- pulp/tests/test_pulp.py
+++ pulp/tests/test_pulp.py
@@ -1068,6 +1068,26 @@ class BaseSolverTest:
             print("\t Testing reading MPS files - maximize")
             self.assertDictEqual(_dict1, _dict2)
 
+        def test_importMPS_noname(self):
+            name = self._testMethodName
+            prob = LpProblem("", const.LpMaximize)
+            x = LpVariable("x", 0, 4)
+            y = LpVariable("y", -1, 1)
+            z = LpVariable("z", 0)
+            w = LpVariable("w", 0)
+            prob += x + 4 * y + 9 * z, "obj"
+            prob += x + y <= 5, "c1"
+            prob += x + z >= 10, "c2"
+            prob += -y + z == 7, "c3"
+            prob += w >= 0, "c4"
+            filename = name + ".mps"
+            prob.writeMPS(filename)
+            _vars, prob2 = LpProblem.fromMPS(filename, sense=prob.sense)
+            _dict1 = getSortedDict(prob)
+            _dict2 = getSortedDict(prob2)
+            print("\t Testing reading MPS files - noname")
+            self.assertDictEqual(_dict1, _dict2)
+
         def test_importMPS_integer(self):
             name = self._testMethodName
             prob = LpProblem(name, const.LpMinimize)
