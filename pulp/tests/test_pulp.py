0a4ad4e95ceb7741bca1d4d794b6d46dd004e838
nariaki3551 <nariaki3551@gmail.com>
2022-08-25 10:07:37 +0900

    update unit test

--- pulp/tests/test_pulp.py
+++ pulp/tests/test_pulp.py
@@ -86,6 +86,7 @@ class BaseSolverTest:
             assert dict(lpDot([x, y, z], a)) == {x: 1, y: 2, z: 3}
             assert dict(lpDot([2*x, 2*y, 2*z], a)) == {x: 2, y: 4, z: 6}
             assert dict(lpDot([x+y, y+z, z], a)) == {x: 1, y: 3, z: 5}
+            assert dict(lpDot(a, [x+y, y+z, z])) == {x: 1, y: 3, z: 5}
 
         def test_pulp_009(self):
             # infeasible
