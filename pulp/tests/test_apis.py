f2b8e8ebff98cb4843f0573c8a5cc1d0b7573f42
Tim Jentzsch <tim-jentzsch@gmx.de>
2023-01-28 15:53:54 +0100

    Add tests for FSCIP API

--- pulp/tests/test_apis.py
+++ pulp/tests/test_apis.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pulp import SCIP_CMD
+from pulp import SCIP_CMD, FSCIP_CMD
 
 
 class ScipTest(unittest.TestCase):
@@ -25,36 +25,35 @@ class ScipTest(unittest.TestCase):
             'write solution "tmpSol"',
             "-c",
             "quit",
-        ]
+        ], f"Command is not equal: {command}"
 
-        assert file_options == []
+        assert file_options == [], f"File options are not equal: {file_options}"
 
 
 class FscipTest(unittest.TestCase):
     def testBuildSolveCommand(self):
-        scip = SCIP_CMD(options=["-c", "set presolving emphasis aggressive"])
+        fscip = FSCIP_CMD(options=["-sl", "foo"])
 
-        command, file_options = scip._build_solve_command(
-            "tmpLp", "tmpSol", "tmpOptions"
+        command, file_options, file_parameters = fscip._build_solve_command(
+            "tmpLp", "tmpSol", "tmpOptions", "tmpParams"
         )
 
         assert command == [
-            scip.path,
+            fscip.path,
+            "tmpParams",
+            "tmpLp",
             "-s",
             "tmpOptions",
-            "-c",
-            "set presolving emphasis aggressive",
-            "-c",
-            'read "tmpLp"',
-            "-c",
-            "optimize",
-            "-c",
-            'write solution "tmpSol"',
-            "-c",
-            "quit",
-        ]
+            "-fsol",
+            "tmpSol",
+            "-sl",
+            "foo",
+        ], f"Command is not equal: {command}"
 
-        assert file_options == []
+        assert file_options == [], f"File options are not equal: {file_options}"
+        assert file_parameters == [
+            "NoPreprocessingInLC = TRUE"
+        ], f"File parameters are not equal: {file_parameters}"
 
 
 if __name__ == "__main__":
