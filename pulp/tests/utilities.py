29ab059aec314ff9479056edddeb401e1ae2354d
Marcus Reaiche <marcusreaiche@gmail.com>
2024-10-02 22:19:40 -0400

    Remove type annotations and add docstring

--- pulp/tests/utilities.py
+++ pulp/tests/utilities.py
@@ -1,7 +1,7 @@
 import re
 
 
-def read_command_line_from_log_file(logPath: str) -> str:
+def read_command_line_from_log_file(logPath):
     """
     Read from log file the command line executed.
     """
@@ -13,8 +13,28 @@ def read_command_line_from_log_file(logPath: str) -> str:
 
 
 def extract_option_from_command_line(
-    command_line: str, option: str, prefix: str = "-", grp_pattern: str = "[a-zA-Z]+"
-) -> str:
+    command_line, option, prefix="-", grp_pattern="[a-zA-Z]+"
+):
+    """
+    Extract option value from command line string.
+
+    :param command_line: str that we extract the option value from
+    :param option: str representing the option name (e.g., presolve, sec, etc)
+    :param prefix: str (default: '-')
+    :param grp_pattern: str (default: '[a-zA-Z]+') - regex to capture option value
+
+    :return: option value captured (str); otherwise, None
+
+    example:
+
+    >>> cmd = "cbc model.mps -presolve off -timeMode elapsed -branch"
+    >>> extract_option_from_command_line(cmd, "presolve")
+    'off'
+
+    >>> cmd = "cbc model.mps -strong 101 -timeMode elapsed -branch"
+    >>> extract_option_from_command_line(cmd, "strong", grp_pattern="\d+")
+    '101'
+    """
     pattern = re.compile(rf"{prefix}{option}\s+({grp_pattern})\s*")
     m = pattern.search(command_line)
     if not m:
