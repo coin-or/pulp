0d6e0a5400b0690564fe6576945358645e8fb5eb
Ishika Roy <41401566+Iroy30@users.noreply.github.com>
2025-05-29 14:50:13 +0530

    CuOpt Solver Integration (#839)
    * add cuopt solver interface
    
    * fix mip
    
    * update constraint extraction and tests
    
    * incorporate review changes
    
    * Update cuopt_api.py
    
    * Delete pulp/tests/test_pulp_temp.py
    
    * run mypy
    
    * Update cuopt_api.py

--- pulp/tests/test_pulp.py
+++ pulp/tests/test_pulp.py
@@ -100,7 +100,11 @@ class BaseSolverTest:
         solveInst: Optional[Type[LpSolver]] = None
 
         def setUp(self):
-            self.solver = self.solveInst(msg=False)
+            if self.solveInst == CUOPT:
+                # cuOpt requires a user provided time limit for MIP problems
+                self.solver = self.solveInst(msg=False, timeLimit=120)
+            else:
+                self.solver = self.solveInst(msg=False)
             if not self.solver.available():
                 self.skipTest(f"solver {self.solveInst.name} not available")
 
@@ -2231,6 +2235,10 @@ class HiGHS_CMDTest(BaseSolverTest.PuLPTest):
 
 class COPTTest(BaseSolverTest.PuLPTest):
     solveInst = COPT
+
+
+class CUOPTTest(BaseSolverTest.PuLPTest):
+    solveInst = CUOPT
 
 
 class SASTest:
